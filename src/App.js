2f8e6b0506a3ceadc4302bf5b58a71546f4212dc
Thiago Mota <th.araujomota@gmail.com>
2022-02-23 16:33:19 -0300

    feat: adds props to Form component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <h1>Tryunfo</h1>
         <Form />
       </div>
     );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,77 +1,130 @@
-import { number } from 'prop-types';
+import propTypes from 'prop-types';
 import React from 'react';
 
 class Form extends React.Component {
   render() {
+    const {
+      cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      onInputChange, onSaveButtonClick,
+    } = this.props;
+
     return (
       <form>
 
-        Nome da carta
-        <input
-          type="text"
-          data-testid="name-input"
-          placeholder="Insira o nome da carta"
-        />
+        <label htmlFor="name">
+          <input
+            type="text"
+            data-testid="name-input"
+            placeholder="Insira o nome da carta"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
+        </label>
 
-        Descrição
-        <input
-          type="textarea"
-          data-testid="description-input"
-          placeholder="Insira a descrição da carta"
-        />
+        <label htmlFor="description">
+          <input
+            type="textarea"
+            data-testid="description-input"
+            placeholder="Insira a descrição da carta"
+            value={ cardDescription }
+            onChange={ onInputChange }
+          />
+        </label>
 
-        Attr01
-        <input
-          type="number"
-          data-testid="attr1-input"
-          placeholder="1º atributo"
-        />
+        <label htmlFor="attr1">
+          <input
+            type="number"
+            data-testid="attr1-input"
+            placeholder="1º atributo"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
+        </label>
 
-        Attr02
-        <input
-          type="number"
-          data-testid="attr2-input"
-          placeholder="2º atributo"
-        />
+        <label htmlFor="attr2">
+          <input
+            type="number"
+            data-testid="attr2-input"
+            placeholder="2º atributo"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
+        </label>
 
-        Attr03
-        <input
-          type="number"
-          data-testid="attr3-input"
-          placeholder="3º atributo"
-        />
+        <label htmlFor="attr3">
+          <input
+            type="number"
+            data-testid="attr3-input"
+            placeholder="3º atributo"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
+        </label>
 
-        Imagem
-        <input
-          type="text"
-          data-testid="image-input"
-          placeholder="Caminho da imagem"
-        />
+        <label htmlFor="image">
+          <input
+            type="text"
+            data-testid="image-input"
+            placeholder="Caminho da imagem"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
+        </label>
 
-        Raridade
-        <select data-testid="rare-input">
-          <option value="normal">Normal</option>
-          <option value="raro">Raro</option>
-          <option value="muito raro">Muito Raro</option>
-        </select>
+        <label htmlFor="rare">
+          <select
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muito raro">Muito Raro</option>
+          </select>
+        </label>
 
-        Super Trybe Trunfo
-        <input
-          type="checkbox"
-          data-testid="trunfo-input"
-          value="É Super Trunfo?"
-        />
+        <label htmlFor="trunfo">
+          <input
+            type="checkbox"
+            data-testid="trunfo-input"
+            value={ cardTrunfo }
+            onChange={ onInputChange }
+            checked
+          />
+        </label>
 
-        <input
+        <button
           type="button"
           data-testid="save-button"
-          value="Salvar"
-        />
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+
+        >
+          Salvar
+        </button>
+
+        {/* https://tutorial.eyehunts.com/js/add-disabled-attribute-javascript-disable-html-elements-example/ */}
 
       </form>
 
     );
   }
 }
+
+Form.propTypes = {
+  cardName: propTypes.string.isRequired,
+  cardDescription: propTypes.string.isRequired,
+  cardAttr1: propTypes.string.isRequired,
+  cardAttr2: propTypes.string.isRequired,
+  cardAttr3: propTypes.string.isRequired,
+  cardImage: propTypes.string.isRequired,
+  cardRare: propTypes.string.isRequired,
+  cardTrunfo: propTypes.bool.isRequired,
+  hasTrunfo: propTypes.bool.isRequired,
+  isSaveButtonDisabled: propTypes.bool.isRequired,
+  onInputChange: propTypes.func.isRequired,
+  onSaveButtonClick: propTypes.func.isRequired,
+};
 
 export default Form;
